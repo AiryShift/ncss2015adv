63adeadbe8b55dbccd00abc0a3f4df05a132e868
AiryShift <religionofaperturelabs@gmail.com>
2015-08-09 02:34:33 +1000

    Update Ascending Lines.py

--- Week1/Ascending Lines.py
+++ Week1/Ascending Lines.py
@@ -1,13 +1,10 @@
 def isAscending(line):
-  line = line.split()
-  if line != sorted(line) or len(line) != len(set(line)):
-    return False
-  return True
+    line = line.split()
+    if line != sorted(line) or len(line) != len(set(line)):
+        return False
+    return True
 
 with open('lines.txt') as f:
-  lineNum = 1
-  for line in f:
-    # print('Checking line {}'.format(line))
-    if isAscending(line):
-      print('Line {} is ascending.'.format(lineNum))
-    lineNum += 1
+    for lineNum, line in enumerate(f):
+        if isAscending(line):
+            print('Line {} is ascending.'.format(lineNum + 1))
